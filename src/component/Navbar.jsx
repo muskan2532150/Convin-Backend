713b16bddafcb0ddbb29e8a708a4095f0054f482
muskan2532150 <rg2532150@gmail.com>
2023-10-16 10:50:59 +0530

    Add Link to Logo

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-black text-white p-4">
       <div className="container mx-auto flex items-center justify-between">
-        <h1 className="text-2xl font-bold text-yellow-400">BookCar</h1>
+        <Link to="/" className="text-2xl font-bold text-yellow-400">
+          BookCar
+        </Link>
       </div>
     </nav>
   );
