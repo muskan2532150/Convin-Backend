6d3200aee19e5f002473d6b2dad5765076ab37fe
muskan2532150 <rg2532150@gmail.com>
2023-10-16 10:29:17 +0530

    Update Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-        
-    </nav>  )
-}
+    <nav className="bg-black text-white p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <h1 className="text-2xl font-bold text-yellow-400">BookCar</h1>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
